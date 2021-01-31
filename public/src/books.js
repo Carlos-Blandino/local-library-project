c3d86fb68fc01afd20ede21b1f0cac55ae679b5a
Carlos Blandino <cbwebpro.online>
2021-01-31 12:36:03 -0800

    Refactoring: variable names, adding comments, modifying getBorrowersForBook function

--- public/src/books.js
+++ public/src/books.js
@@ -7,22 +7,30 @@ function findBookById(books, id) {
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  const borrowed = books.filter((books) => books.borrows[0].returned === false);
-  const returned = books.filter((books) => books.borrows[0].returned === true);
+  // obtain array of unreturned books
+  const borrowed = books.filter((book) => book.borrows[0].returned === false);
+  //obtain array of returned books
+  const returned = books.filter((book) => books.borrow[0].returned === true);
+  // resulting array will hold an array of book objects of unreturned book and 
+  // and hold a second array of returned book objects
   return [borrowed, returned];
 }
 
 function getBorrowersForBook(book, accounts) {
-  const results = [];
-  book.borrows.forEach((element) => {
-    const acc = accounts.find((account) => account.id === element.id);
-    results.push({
-      ...element,
-      ...acc,
-    });
-  });
+  // filter out each  borrow from the book borrows array that has matchihg id to the account id
+  // and map it out 
+  const bookBorrowers = book.borrows.filter((borrowElement) => {
+    const account = accounts.find((account) => account.id === borrowElement.id)
+    return account.id === borrowElement.id
+  }).map((borrowElement) => {
+    const account = accounts.find((account) => account.id === borrowElement.id);
+    return {
+      ...borrowElement,
+      ...account
+    }
+  })
 
-  return results.slice(0, 10);
+  return bookBorrowers.slice(0, 10);
 }
 
 module.exports = {
